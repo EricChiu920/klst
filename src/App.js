670eb16b1333dfcd4d372467143f2ae6f119531e
IrieY56 <IrieY56@gmail.com>
2018-07-10 18:33:13 -0400

    App.js is not a stateless component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Description from './Description';
 import NumTerms from './NumTerms';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Description />
-        <NumTerms />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+    </header>
+    <Description />
+    <NumTerms />
+  </div>
+)
 
 export default App;
